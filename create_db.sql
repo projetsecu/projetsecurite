35cee54cdad922ba40e64e0d24aa7b4b114fde73
projetsecu <93837180+projetsecu@users.noreply.github.com>
2021-11-28 00:05:53 +0100

    Add files via upload

---
+++ create_db.sql
@@ -0,0 +1,16 @@
+USE scripts
+
+CREATE TABLE script (
+    nom_script VARCHAR(50) PRIMARY KEY,
+    path_script VARCHAR(200),
+    text_script TEXT
+    );
+
+INSERT INTO script (nom_script,path_script,text_script)
+VALUES
+('show_in_console.sh','/home/debian/protected_script','#!/bin/bash \n \n'),
+('clock.sh','/home/debian/protected_script','#!/bin/bash date +%R >> "/home/debian/clock.txt"'),
+('kietu.sh','/home/debian/protected_script','#!/bin/bash whoami >> "/home/debian/clock.txt"');
+
+
+
